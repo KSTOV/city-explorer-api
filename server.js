d079fdebbeec698c3b7b150e2f9773426a1f517f
KLan Stovall <kland13@gmail.com>
2021-11-04 03:42:17 -0700

    lab 08

--- server.js
+++ server.js
@@ -3,48 +3,78 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const weather = require('./data/weather.json');
+const axios = require('axios');
 const app = express();
 app.use(cors());
+
 const PORT = process.env.PORT;
-
-app.get('./data/weather.json');
-app.get('/weather',getWeather);
-app.get('/*', (req, res) => res.status(403).send('not found'));
-
+const weatherAPI = process.env.WEATHER_API_KEY;
+const movieAPI = process.env.MOVIE_API_KEY;
 
 class Forecast {
     constructor(obj) {
         this.date = obj.valid_date;
         this.description = obj.weather.description;
+        this.clouds = obj.clouds;
+        this.maxTemp = obj.max_temp;
+        this.lowTemp = obj.low_temp;
     }
 }
 
-function getWeather(req, res) {
-    let searchQuery = req.query.city;
+let getWeather = async (req, res) => {
+    
     let lat = req.query.lat;
     let lon = req.query.lon;
-
+    
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${weatherAPI}`;
+    
     try {
-        const cityToSend = weather.find(city => {
-            if((city.lat === lat && city.lon === lon) || city.city_name === searchQuery) {
-                return true;
-            }
-            return false;
-        });
-        if (cityToSend) {
-            const forecastData = cityToSend.data.map(city => new Forecast(city));
-            res.status(200).send(forecastData);
+        let response = await axios.get(url);
+        let forcastData = response.data.data.map(forcast => new Forecast(forcast));
+        console.log(forcastData);
+        if(forcastData) {
+            res.status(200).send(forcastData)
         } else {
-            res.status(404).send('Not Found');
+            res.status(404).send('IT NOT WORK  ¯\_(ツ)_/¯ ')
         }
-    } catch (e) {
-        res.status(500).send('server error');
+    } catch {
+        return res.status(500).send('WEATHER DATA NOT FOUND.');
     }
 }
 
+class Movies {
+    constructor(obj) {
+        this.title = obj.title;
+        this.overview = obj.overview;
+        this.average_votes = obj.vote_average;
+        this.total_votes = obj.vote_count;
+        this.image_url = `https://image.tmdb.org/t/p/w500${obj.poster_path}`
+        this.popularity = obj.popularity;
+        this.released_date = obj.released_date;
+    }
+}
+
+let getMovies = async (req, res) => {
+
+    let movieName = req.query.keyword;
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${movieAPI}&query=${movieName}`;
+    
+    try {
+        let response = await axios.get(url);
+        let movieData = response.data.results.map(movie => new Movies(movie));
+        console.log(movieData);
+        if(movieData) {
+            res.status(200).send(movieData)
+        } else {
+            res.status(404).send('IT NOT WORK  ¯\_(ツ)_/¯ ')
+        }
+    } catch {
+        return res.status(500).send('MOVIE DATA NOT FOUND.');
+    }
+}
+app.get('/weather', getWeather);
+app.get('/movies', getMovies);
+app.get('/*', (req, res) => res.status(404).send('NOT FOUND'));
+
 app.listen(PORT, () => console.log(`Listening at port:${PORT}`));
-
-
-
-
