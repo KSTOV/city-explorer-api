a3d4ef82e17c715b1ea5b37d9dc125304d2f2ad6
KLan Stovall <kland13@gmail.com>
2021-11-07 15:01:06 -0800

    lab10

--- movie.js
+++ movie.js
@@ -1,6 +1,7 @@
-'use strict'
+'use strict';
 require('dotenv').config();
 const axios = require('axios');
+const cache = require('./cache.js');
 
 const movieAPI = process.env.MOVIE_API_KEY;
 
@@ -18,20 +19,26 @@ let getMovies = async (req, res) => {
 
     let movieName = req.query.query;
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${movieAPI}&query=${movieName}&page=1`;
-    
-    try {
-        let result = await axios(url);
-        let movieData = result.data.results.map(movie => new Movies(movie));
-        console.log(movieData);
-        if(movieData) {
-            res.status(200).send(movieData)
-        } else {
-            res.status(404).send('IT NOT WORK  ¯\_(ツ)_/¯ ')
+    if(cache[movieName] && (Date.now() - cache[movieName].timestamp) < 200000) {
+        res.status(200).send(cache[movieName]);
+    } else {
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${movieAPI}&query=${movieName}&page=1`;
+        try {
+            let result = await axios(url);
+            let movieData = result.data.results.map(movie => new Movies(movie));
+            console.log(movieData);
+            if(movieData) {
+                cache[movieName] = movieData;
+                cache[movieName].timestamp = Date.now();
+                res.status(200).send(movieData)
+            } else {
+                res.status(404).send('IT NOT WORK  ¯\_(ツ)_/¯ ')
+            }
+        } catch {
+            return res.status(500).send('MOVIE DATA NOT FOUND.');
         }
-    } catch {
-        return res.status(500).send('MOVIE DATA NOT FOUND.');
     }
+
 }
 
 module.exports = getMovies;
--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 'use strict';
 require('dotenv').config();
 const axios = require('axios');
+const cache = require('./cache.js');
 
 const weatherAPI = process.env.WEATHER_API_KEY;
 
@@ -20,22 +21,28 @@ let getWeather = async (req, res) => {
     
     let lat = req.query.lat;
     let lon = req.query.lon;
-    
-    const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${weatherAPI}&days=10`;
-    
-    try {
-        let result = await axios(url);
-        console.log(result);
-        let forcastData = result.data.data.map(forcast => new Forecast(forcast));
-        console.log(forcastData);
-        if(forcastData) {
-            res.status(200).send(forcastData);
-        } else {
-            res.status(404).send('IT NOT WORK  ¯\_(ツ)_/¯ ');
+
+    if(cache[lat, lon] && (Date.now() - cache[lat, lon].timestamp) < 200000) {
+        res.status(200).send(cache[lat, lon]);
+    } else {
+        const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${weatherAPI}&days=10`;
+        try {
+            let result = await axios(url);
+            console.log(result);
+            let forcastData = result.data.data.map(forcast => new Forecast(forcast));
+            console.log(forcastData);
+            if(forcastData) {
+                cache[lat, lon] = forcastData;
+                cache[lat, lon].timestamp = Date.now();
+                res.status(200).send(forcastData);
+            } else {
+                res.status(404).send('IT NOT WORK  ¯\_(ツ)_/¯ ');
+            }
+        } catch {
+            return res.status(500).send('WEATHER DATA NOT FOUND.');
         }
-    } catch {
-        return res.status(500).send('WEATHER DATA NOT FOUND.');
     }
+    
 }
 
 module.exports = getWeather;
